1cd94e378bad6024cd85b84215a67f4a6447ddb9
fengjiangtao <fengjiangt@139.com>
2018-09-09 22:11:57 +0800

    Initial commit

---
+++ src/main/java/cn/jeremy/marry/MarryApplication.java
@@ -0,0 +1,12 @@
+package cn.jeremy.marry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarryApplication.class, args);
+	}
+}
